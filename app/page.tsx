535f420c566b87fbae9125f8db5619f5222e8175
yoel-coder <yoellef218@gmail.com>
2026-08-16 13:25:21 -0400

    Remove TypeScript project file app/page.tsx

--- app/page.tsx
+++
@@ -1,4 +0,0 @@
-import TaskBoard from "../components/TaskBoard";
-import { requireChatGPTUser } from "./chatgpt-auth";
-export const dynamic="force-dynamic";
-export default async function Home(){const user=await requireChatGPTUser("/");return <TaskBoard name={user.displayName}/>}
